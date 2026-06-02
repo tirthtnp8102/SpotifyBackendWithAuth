9325c88160e1cdbd6cb2bbccd200fbdeb8eff992
Tirth Patel <tnptirth1@gmail.com>
2026-06-02 23:49:52 +0530

    Added User Auth Middleware to authorise asrtists and user for their respective roles

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -82,4 +82,9 @@ async function loginUser(req, res) {
 
 }
 
-module.exports = { registerUser, loginUser };
+async function logoutUser(req, res) {
+    res.clearCookie("token")
+    res.status(200).json({ message: "User logged out successfully" });
+}
+
+module.exports = { registerUser, loginUser, logoutUser };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,6 +4,7 @@ const authController = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/register', authController.registerUser);
-router.get('/login', authController.loginUser);
+router.post('/login', authController.loginUser);
+router.post('/logout', authController.logoutUser);
 
 module.exports = router;
